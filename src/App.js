f1c461bc0b9a453d8f2b6693a7d1d72d77aa92c4
Bipin MV <bipinmv23@gmail.com>
2021-06-27 12:39:26 +0530

    added utils file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
 import './App.css';
-import Video from './components/Video'
+import Video from './components/Video';
+import { videoSource } from "./utils/videoSource";
 
 function App() {
   return (
     <div className="App">
       <div className="video-container">
-          <Video
-            url="https://assets.mixkit.co/videos/preview/mixkit-hand-holding-a-multicolored-smoke-bomb-2825-large.mp4"
-            name="Hand holding a multicolored smoke bomb"/>
-          <Video
-            url="https://assets.mixkit.co/videos/preview/mixkit-tree-with-yellow-flowers-1173-large.mp4"
-            name="Wind" />
-          <Video
-            url="https://assets.mixkit.co/videos/preview/mixkit-palm-frond-lifeguard-station-1194-large.mp4"
-            name="Palm frond lifeguard station"  />
-          <Video
-            url="https://assets.mixkit.co/videos/preview/mixkit-air-bubbles-underwater-upside-video-1273-large.mp4"
-            name="Air bubbles underwater, upside video"/>
-            <Video
-              url="https://assets.mixkit.co/videos/preview/mixkit-basketball-player-dribbling-then-dunking-2285-large.mp4"
-              name="Basketball player dribbling then dunking"/>
+        {videoSource.map(obj => <Video url={obj.url} name={obj.name} />)}
       </div>
     </div>
   );
---
+++ src/utils/videoSource.js
@@ -0,0 +1,16 @@
+export const videoSource = [{
+    url: "https://assets.mixkit.co/videos/preview/mixkit-hand-holding-a-multicolored-smoke-bomb-2825-large.mp4",
+    name: "Hand holding a multicolored smoke bomb"
+}, {
+    url: "https://assets.mixkit.co/videos/preview/mixkit-tree-with-yellow-flowers-1173-large.mp4",
+    name: "Wind"
+}, {
+    url: "https://assets.mixkit.co/videos/preview/mixkit-palm-frond-lifeguard-station-1194-large.mp4",
+    name: "Palm frond lifeguard station"
+}, {
+    url: "https://assets.mixkit.co/videos/preview/mixkit-air-bubbles-underwater-upside-video-1273-large.mp4",
+    name: "Air bubbles underwater, upside video"
+}, {
+    url: "https://assets.mixkit.co/videos/preview/mixkit-basketball-player-dribbling-then-dunking-2285-large.mp4",
+    name: "Basketball player dribbling then dunking"
+}];
