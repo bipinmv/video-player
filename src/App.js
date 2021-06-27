7009c964513a33f00cf766faa97c750fec897f8c
Bipin MV <bipinmv23@gmail.com>
2021-06-27 12:59:25 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import './App.css';
 import Video from './components/Video';
 import { videoSource } from "./utils/videoSource";
+import { BrowserRouter } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <div className="video-container">
-        {videoSource.map(obj => <Video url={obj.url} name={obj.name} />)}
+    <BrowserRouter basename="/video-player">
+      <div className="App">
+        <div className="video-container">
+          {videoSource.map(obj => <Video url={obj.url} name={obj.name} />)}
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
